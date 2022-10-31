5c4cf4188e97e1cc5d7e0c26b122c313b77d154f
dyadyajora <ja_massa97@mail.ru>
2022-11-01 02:15:14 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,302 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
+const NICKNAME = process.env.TELEGRAM_BOT_NICKNAME || 'YOUR_TELEGRAM_BOT_NICKNAME';
+const SQLITE_DB_PATH = process.env.SQLITE_DB_PATH || './db.sqlite3';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: SQLITE_DB_PATH
+});
+const Subscriptions = sequelize.define('Subscriptions', {
+    chatId: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    chatTitle: {
+        type: DataTypes.STRING
+    },
+    ownerId: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    ownerName: {
+        type: DataTypes.STRING
+    },
+    message: {
+        type: DataTypes.STRING
+    }
+}, {});
+
+class App {
+    constructor() {
+        this.bot = new TelegramBot(TOKEN, {polling: true});
+    }
+
+    start() {
+        this.bot.on('message', (msg) => {
+            const chatId = msg.chat.id;
+
+            console.log(msg);
+
+            if (msg.group_chat_created) {
+                this.bot.sendMessage(chatId, '\u{1F44B} Всем привет в новом чате! Что я умею: /help')
+                    .catch(err => {
+                        console.log("Couldn't send first message", err);
+                    });
+                return;
+            }
+
+            if (msg.left_chat_participant) {
+                this._deleteWelcomeMessageByChatId(chatId)
+                    .then(() => {
+                        console.log("processed deleted from chat");
+                    })
+                    .catch(err => {
+                        console.log("error deleting from chat: " + chatId, err);
+                    });
+                return;
+            }
+
+            if (!msg.new_chat_participant) {
+                return;
+            }
+
+            this._getSubscription(chatId)
+                .then(sub => {
+                    if (!sub) {
+                        this._sendNoDataMessage(chatId);
+                        return;
+                    }
+
+                    let content = sub['message'];
+                    if (!content) {
+                        this._sendNoDataMessage(chatId);
+                        return;
+                    }
+                    let newMembersStr = msg.new_chat_members.map(member => {
+                        return this._buildOwnerName(member);
+                    }).join(", ")
+
+                    let chatTitle = msg.chat.title;
+
+                    let welcomeText = 'Добро пожаловать в чат "' + chatTitle + '", ' + newMembersStr + '!\n';
+                    let welcomeMessage = welcomeText + content;
+
+                    return this.bot.sendMessage(chatId, welcomeMessage);
+                })
+                .catch(err => {
+                    console.log(err, "Couldn't send welcome message");
+                });
+        });
+
+        const helpRegexp = new RegExp('^\/help(|@' + NICKNAME + ')$');
+        this.bot.onText(helpRegexp, (msg, match) => {
+            const chatId = msg.chat.id;
+            let helpMessage = "\u{1F7E2} <b>Зеленый бот для приветствия при добавлении в чат</b> \u{1F7E2}\n\n" +
+                "/start - запустить(перезапустить бота)\n" +
+                "/set_text TEXT - установить текст приветствия\n" +
+                "/get_text - напечатать текст приветствия\n" +
+                "/owner - узнать кто может редактировать приветствие\n" +
+                "/help напечатать это сообщение\n\n\u{270C}";
+
+            this.bot.sendMessage(chatId, helpMessage, {
+                parse_mode: 'html'
+            });
+        })
+
+        const startRegexp = new RegExp('^\/start(|@' + NICKNAME + ')$');
+        this.bot.onText(startRegexp, (msg, match) => {
+            const chatId = msg.chat.id;
+
+            this._getSubscription(chatId)
+                .then(sub => {
+                    if (sub !== null) {
+                        this.bot.sendMessage(chatId, "Бот уже запущен.");
+                        return;
+                    }
+
+                    let ownerId = msg.from.id;
+                    let ownerName = this._buildOwnerName(msg.from);
+                    let chatTitle = (msg.chat.title || msg.chat.type);
+                    this._registerNewChat(chatId, chatTitle, ownerId, ownerName)
+                        .then((sub) => {
+                            console.log(sub, "subscription recreated!");
+                            return this.bot.sendMessage(chatId, 'Я стартовал! Можете задать текст приветствия через команду /set_text приветствие.\nПодробнее о командах: /help');
+                        })
+                        .catch((err) => {
+                            console.log("Couldn't send first message", err);
+                            return this.bot.sendMessage(chatId, 'Что-то пошло не так. Попробуйте удалить бота из чата и добавить заново.');
+                        });
+                })
+        });
+
+        const ownerRegexp = new RegExp('^\/owner(|@' + NICKNAME + ')$');
+        this.bot.onText(ownerRegexp, (msg, match) => {
+            const chatId = msg.chat.id;
+
+            this._getSubscription(chatId)
+                .then(sub => {
+                    let ownerName = sub['ownerName'];
+                    return this.bot.sendMessage(chatId, "Мой хозяин в этом чате: " + ownerName);
+                })
+                .catch(err => {
+                    console.log(err, "Error getting owner name");
+                })
+        });
+
+        const getTextRegexp = new RegExp('^\/get_text(|@' + NICKNAME + ')$');
+        this.bot.onText(getTextRegexp, (msg, match) => {
+            const chatId = msg.chat.id;
+
+            this._getSubscription(chatId)
+                .then(sub => {
+                    if (sub === null) {
+                        this.bot.sendMessage(chatId, "К сожалению, что-то пошло не так\nПожалуйста вызовите команду /start и после этого задайте текст приветствия заново.");
+                        return;
+                    }
+
+                    let content = sub['message'];
+                    if (!content) {
+                        return this.bot.sendMessage(chatId, "\u{1F6AB} *Текст приветствия не установлен*", {
+                            parse_mode: 'markdown'
+                        })
+                    }
+
+                    return this.bot.sendMessage(chatId, content);
+                })
+                .catch(err => {
+                    console.log("Couldn't send getText message", err);
+                });
+        });
+
+        const setTextRegexp = new RegExp('^\/set_text(|@' + NICKNAME + ') (((.*)|([\\n\\r]*))*)$');
+        this.bot.onText(setTextRegexp, (msg, match) => {
+            const chatId = msg.chat.id;
+            let newWelcomeMessage = match[2];
+            if (!newWelcomeMessage) {
+                this.bot.sendMessage(chatId, "\u{1F6AB} Невозможно установить пустое сообщение");
+                return;
+            }
+
+            let requesterId = msg.from.id;
+
+            this._setWelcomeMessageByChatId(chatId, requesterId, newWelcomeMessage)
+                .then(() => {
+                    console.log("Subscription successfully updated! " + chatId);
+                })
+                .catch((err) => {
+                    console.log(err, "Error updating subscription in chat: " + chatId);
+                })
+        });
+
+        console.log("Bot application started");
+    }
+
+    _sendNoDataMessage(chatId) {
+        return this.bot.sendMessage(chatId, "\u{1F937} Я бы мог поприветствовать этого человека, но меня еще ничему не научили ");
+    }
+
+    _setWelcomeMessageByChatId(chatId, requesterId, content) {
+        return new Promise((resolve, reject) => {
+            this._getSubscription(chatId)
+                .then(sub => {
+                    if (!sub) {
+                        reject();
+                        return;
+                    }
+
+                    let ownerId = sub['ownerId'];
+                    let ownerName = sub['ownerName'];
+                    if (ownerId !== requesterId) {
+                        this.bot.sendMessage(chatId, 'Изменять приветствие может только тот, кто меня добавил.\nВ этот чат меня добавил ' + ownerName);
+                        reject();
+                        return;
+                    }
+
+                    Subscriptions.update({
+                        message: content
+                    }, {
+                        where: {
+                            chatId: chatId
+                        }
+                    }).then(() => {
+                        this.bot.sendMessage(chatId, "\u{2705} Приветствие успешно обновлено!\n<b>Новое приветствие</b>:\n" + content, {
+                            parse_mode: 'html'
+                        }).then(() => {
+                            resolve();
+                        }).catch(err => {
+                            console.log(err, "Error sending ok message!");
+                            reject(err);
+                        });
+                    }).catch(err => {
+                        this.bot.sendMessage(chatId, "\u{274C} Не удалось обновить приветствие. Попробуйте выполнить команду еще раз.");
+                        reject(err);
+                    });
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+    }
+
+    _deleteWelcomeMessageByChatId(chatId) {
+        return Subscriptions.destroy({
+            where: {
+                chatId: chatId
+            }
+        });
+    }
+
+    _registerNewChat(chatId, chatTitle, ownerId, ownerName) {
+        return Subscriptions.create({
+            ownerId: ownerId,
+            ownerName: ownerName,
+            chatId: chatId,
+            chatTitle: chatTitle
+        });
+    }
+
+    _getSubscription(chatId) {
+        return Subscriptions.findOne({
+            where: {
+                chatId: chatId
+            }
+        });
+    }
+
+    _getAllSubscriptions() {
+        return Subscriptions.findAll();
+    }
+
+    _buildOwnerName(from) {
+        let firstName = from.first_name;
+        let lastName = from.last_name;
+        let username = from.username;
+        let msgOwnerNameParts = [];
+        if (!!firstName) {
+            msgOwnerNameParts.push(firstName);
+        }
+        if (!!lastName) {
+            msgOwnerNameParts.push(lastName);
+        }
+        if (!!username) {
+            msgOwnerNameParts.push('@' + username);
+        }
+
+        return msgOwnerNameParts.join(' ');
+    }
+}
+
+const app = new App();
+Subscriptions.sync({ alter: true })
+    .then(() => {
+        app.start();
+    })
+    .catch(err => {
+        console.log(err, "Error connecting database!");
+    })
