37548ec95eee10dd31628f33dcfdb312dfe30fe2
dyadyajora <ja_massa97@mail.ru>
2022-11-06 18:20:06 +0300

    update localization messages

--- data/i18n.js
+++ data/i18n.js
@@ -12,71 +12,71 @@ module.exports = {
         'ru-RU': 'Бот уже запущен.'
     },
     STARTED_SUCCESSFULLY: {
-        'en-GB': '',
-        'ru-RU': 'Я стартовал! Можете задать текст приветствия через команду /set_text приветствие.\nПодробнее о командах: /help'
+        'en-GB': 'I started! You can set the greeting text through the command /set_text GREETING.\nTo learn more about commands: /help',
+        'ru-RU': 'Я стартовал! Можете задать текст приветствия через команду /set_text ПРИВЕТСТВИЕ.\nПодробнее о командах: /help'
     },
     STARTED_WITH_FAIL: {
-        'en-GB': '',
+        'en-GB': 'Something went wrong. Try removing the bot from the chat and adding it again.',
         'ru-RU': 'Что-то пошло не так. Попробуйте удалить бота из чата и добавить заново.'
     },
     OWNER_TEXT: {
-        'en-GB': '',
+        'en-GB': 'My owner in this chat: ',
         'ru-RU': 'Мой хозяин в этом чате: '
     },
     GET_TEXT_ERROR: {
-        'en-GB': '',
+        'en-GB': 'Unfortunately something went wrong\nPlease call the /start command and then set the greeting text again.',
         'ru-RU': 'К сожалению, что-то пошло не так\nПожалуйста вызовите команду /start и после этого задайте текст приветствия заново.'
     },
     GET_TEXT_EMPTY: {
-        'en-GB': '',
+        'en-GB': 'Welcome text not passed',
         'ru-RU': 'Текст приветствия не установлен'
     },
     SET_TEXT_EMPTY: {
-        'en-GB': '',
+        'en-GB': 'Can\'t set empty message',
         'ru-RU': 'Невозможно установить пустое сообщение'
     },
     SET_TEXT_PERMISSION_ERROR: {
-        'en-GB': '',
+        'en-GB': 'Only the person who added me can change the greeting.\nIn this chat I was added by',
         'ru-RU': 'Изменять приветствие может только тот, кто меня добавил.\nВ этот чат меня добавил'
     },
     SET_TEXT_UPDATED_SUCCESSFULLY: {
-        'en-GB': '',
+        'en-GB': 'Greeting updated successfully!',
         'ru-RU': 'Приветствие успешно обновлено!'
     },
     SET_TEXT_NEW_TEXT: {
-        'en-GB': '',
+        'en-GB': 'New greeting',
         'ru-RU': 'Новое приветствие'
     },
     SET_TEXT_UPDATED_WITH_FAIL: {
-        'en-GB': '',
+        'en-GB': 'Failed to update greeting. Try running the command again.',
         'ru-RU': 'Не удалось обновить приветствие. Попробуйте выполнить команду еще раз.'
     },
     WELCOME_NO_DATA: {
-        'en-GB': '',
+        'en-GB': 'I could greet this man, but I haven\'t been taught anything yet',
         'ru-RU': 'Я бы мог поприветствовать этого человека, но меня еще ничему не научили'
     },
     HELP_HEADER: {
-        'en-GB': '',
+        'en-GB': 'Bot for greeting new members when added to chat',
         'ru-RU': 'Бот для приветствия при добавлении в чат'
     },
     HELP_START: {
-        'en-GB': '',
+        'en-GB': 'run (restart the bot)',
         'ru-RU': 'запустить(перезапустить бота)'
     },
     HELP_GET_TEXT: {
-        'en-GB': '',
+        'en-GB': 'print greeting message',
         'ru-RU': 'напечатать текст приветствия'
     },
     HELP_SET_TEXT: {
-        'en-GB': '',
+        'en-GB': 'set new greeting message',
         'ru-RU': 'установить текст приветствия'
     },
     HELP_OWNER: {
-        'en-GB': '',
+        'en-GB': 'to know who can edit greeting message',
         'ru-RU': 'узнать кто может редактировать приветствие'
     },
     HELP_HELP: {
-        'en-GB': '',
+        'en-GB': 'print this message',
         'ru-RU': 'напечатать это сообщение'
     }
 }
